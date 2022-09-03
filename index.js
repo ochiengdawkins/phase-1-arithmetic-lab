cd07abf6c74b49381055b75ffbf7ed8a67a3efc4
DawkinsJonathanOchieng <dawkinsjonathan871@gmail.com>
2022-09-03 10:06:22 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 62;
+const num2 = 1;
+const num3 = 24;
+const num4 = 5;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10 ) + 1;
+
+let mod = num3 % num4;
+
+let max = Math.max(1 ,2 ,10, 15, 20);
